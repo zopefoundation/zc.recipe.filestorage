0b328ac0020eb9803087ee13efb53dde5c0c4dee
Christian Zagrodnick <cz@gocept.com>
2007-06-14 11:08:46 +0000

    setting version for the next release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 name = "zc.recipe.filestorage"
 setup(
     name = name,
-    version = "1.0a5",
+    version = "1.0a6",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for defining a file-storage",
