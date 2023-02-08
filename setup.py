c4da441314eefdab5a9ed8a40d3e5bab5e53c9fe
Michael Howitz <mh@gocept.com>
2023-02-08 18:08:35 +0100

    Drop support for Python 2.7 up to 3.6.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name=name,
     version='2.0.dev0',
     author="Jim Fulton",
-    author_email="jim@zope.com",
+    author_email="zope-dev@zope.dev",
     description="ZC Buildout recipe for defining a file-storage",
     long_description=(
         open(os.path.join(
@@ -18,7 +18,7 @@ setup(
         open("CHANGES.rst").read()),
     license="ZPL 2.1",
     keywords=["zodb", "zc.buildout"],
-    url="http://pypi.python.org/pypi/%s/" % name,
+    url="https://pypi.org/project/%s/" % name,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: ZODB",
@@ -41,7 +41,8 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     namespace_packages=["zc", "zc.recipe"],
-    install_requires=["zc.buildout", "zope.testing", "setuptools", "six"],
+    python_requires='>=3.7',
+    install_requires=["zc.buildout", "zope.testing", "setuptools"],
     entry_points={"zc.buildout": ["default=%s:Recipe" % name]},
     include_package_data=True,
     zip_safe=True,
